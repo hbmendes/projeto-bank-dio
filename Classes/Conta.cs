a727cfd3177c1888a716493a7e9de0758e47c79a
Harielson <harielson.mendes@gmail.com>
2021-09-20 10:43:25 -0300

    Primeiro commit

---
+++ Classes/Conta.cs
@@ -0,0 +1,11 @@
+namespace dio.bank
+{
+    public class Conta
+    {
+        private double Saldo {get; set;}
+
+        private double Credito {get; set;}
+
+        private string Nome {get; set;}
+    }
+}
